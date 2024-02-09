8d7b22ce48ccd60d9904971c4f12f3f3d17e9b9b
ErojiAruna <aruna.eroji@gamil.com>
2024-02-09 23:52:25 +0530

    Rendering Lists

--- src/index.js
+++ src/index.js
@@ -25,6 +25,13 @@ const pizzaData = [
     soldOut: false,
   },
   {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
     name: 'Pizza Salamino',
     ingredients: 'Tomato, mozarella, and pepperoni',
     price: 15,
@@ -62,29 +69,34 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizza
         name="Focaccia"
         ingredients="Bread with italian olive oil and rosemary"
-        price="6"
+        price={6}
         photoName="pizzas/focaccia.jpg"
       />
       <Pizza
         name="Pizza Margherita"
         ingredients="Tomato and mozarella"
-        price="10"
+        price={10}
         photoName="pizzas/margherita.jpg"
       />
       <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        price="12"
+        price={12}
         photoName="pizzas/spinaci.jpg"
       />
 
       <Pizza
         name="Pizza Salamino"
         ingredients="Tomato, mozarella, and pepperoni"
-        price="15"
+        price={15}
         photoName="pizzas/salamino.jpg"
       />
       <Pizza
@@ -92,21 +104,21 @@ function Menu() {
         ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
         price={18}
         photoName="pizzas/prosciutto.jpg"
-      />
+      /> */}
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
